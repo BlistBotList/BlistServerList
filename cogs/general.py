d9c5c88ce5e4c14a2dda0c48dd87a4614341b97d
A-Trash-Coder <gavynlamar@gmail.com>
2021-01-23 15:04:05 -0500

    Update command

--- cogs/general.py
+++ cogs/general.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 import os
+import asyncio
 
 class General(commands.Cog):
 
@@ -29,6 +30,58 @@ class General(commands.Cog):
     async def restart(self, ctx):
         await ctx.send("Restarting")
         os.system("systemctl restart blists")
+
+
+    @commands.is_owner()
+    @commands.command()
+    async def update(self, ctx):
+        """Pulls from a git remote and reloads modified cogs"""
+        await ctx.channel.trigger_typing()
+        process = await asyncio.create_subprocess_exec(
+            "git", "pull",
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE
+        )
+
+        try:
+            com = await asyncio.wait_for(process.communicate(), timeout=5)
+            com = com[0].decode() + "\n" + com[1].decode()
+        except asyncio.TimeoutError:
+            await ctx.send("The process timed out.")
+
+        reg = r"\S+(\.py)"
+        reg = re.compile(reg)
+        found = [match.group()[:-3].replace("/", ".")
+                 for match in reg.finditer(com)]
+
+        if found:
+            updated = []
+            for file in found:
+                try:
+                    self.bot.reload_extension(file)
+                    updated.append(file)
+                except (commands.ExtensionNotLoaded, commands.ExtensionNotFound):
+                    continue
+                except Exception as e:
+                    embed = discord.Embed(title=f"There was an issue pulling from GitHub",
+                                          description=f"\n```{e}```\n", color=discord.Color.red())
+                    await ctx.send(embed=embed)
+                    return
+
+            if not updated:
+                embed = discord.Embed(
+                    title=f"No cogs were updated.", color=discord.Color.red())
+                await ctx.send(embed=embed)
+            else:
+                embed = discord.Embed(
+                    title=f"Updated cogs: " +
+                    ", ".join([f"`{text}`" for text in updated]),
+                    color=discord.Color.blurple())
+                await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(
+                title=f"No cogs were updated.", color=discord.Color.red())
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
