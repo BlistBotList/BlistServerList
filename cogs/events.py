558cdb9da59abf6e7acb60cade0a6a9539bb04e7
Dylan <66552108+DylsterDev@users.noreply.github.com>
2021-03-29 15:19:31 +0100

    Fix icon issues. (#1)

--- cogs/events.py
+++ cogs/events.py
@@ -105,10 +105,12 @@ class Events(commands.Cog):
             premium = db_check[0]["premium"]
         except IndexError:
             premium = False
+
+        avatar_hash = guild.icon or "https://blist.xyz/main_site/staticfiles/main/assets/question-mark.png"
             
         managers = " ".join(
             [str(x.id) for x in guild.members if x.guild_permissions.manage_guild and not x.bot])
-        await self.bot.pool.execute("INSERT INTO main_site_server (name, id, main_owner, website, short_description, invite_url, tags, monthly_votes, total_votes, vanity_url, member_count, added, webhook_url, joins, page_views, donate_url, card_background, premium, icon_hash, published, archived, managers) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22)", guild.name, guild.id, guild.owner.id, "", "", "", [""], 0, 0, "", len(guild.members), datetime.datetime.utcnow(), "", 0, 0, "", "", premium, guild.icon, False, False, managers)
+        await self.bot.pool.execute("INSERT INTO main_site_server (name, id, main_owner, website, short_description, invite_url, tags, monthly_votes, total_votes, vanity_url, member_count, added, webhook_url, joins, page_views, donate_url, card_background, premium, icon_hash, published, archived, managers) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22)", guild.name, guild.id, guild.owner.id, "", "", "", [""], 0, 0, "", len(guild.members), datetime.datetime.utcnow(), "", 0, 0, "", "", premium, avatar_hash, False, False, managers)
         logs = self.bot.main_guild.get_channel(793584504291065857)
         embed = discord.Embed(title="New Server Joined!", color=discord.Color.blurple(), description=f"""
 >>> **Name:** {guild.name}
